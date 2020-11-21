694beb0b0fc97ea8aae132f9f88a189a67070548
Florian Schumacher <fpsschumacher@gmail.com>
2020-11-21 15:26:18 +0100

    Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
