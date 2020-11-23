55a2b36f2a276e6c693c2092edccc18a2bafd7f5
Florian Schumacher <fpsschumacher@gmail.com>
2020-11-23 10:31:38 +0100

    Fetches Dummy API, presents Data as list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react'
-import Styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import getDataDummy from './components/services/getDataDummy'
+import ListBox from './components/modules/ListBox'
 
 
 function App() {
+  const [ListBoxes, setListBoxes] = useState([])
+
+  /* Aufruf der Morty API, Variablennamen dürfen nicht mit bereits 
+  deklarierten Namen übereinstimmen, sondern müssen neu vergeben werden */
+
+  useEffect(() => {
+    getDataDummy()
+      .then((data) => setListBoxes(data.results))
+      .catch((error) => console.log(error))
+  }, [])
+
+  /* Ausgabe des API Aufrufs über das ListBox Item, 
+  welches über das entsprechende Modul aufgerufen wurde*/
+
   return (
     <div className="App">
-      Hello World
+      <Contentblock>
+        {ListBoxes.map(({ id, name, image }) => (
+          <ListBox
+            key={id}
+            name={name}
+            imgUrl={image}
+          />
+        ))}
+      </Contentblock>
     </div>
-  );
+  )
 }
 
 export default App;
+
+const Contentblock = styled.div`
+  display: block;
+  margin-top: 80px;
+  margin-bottom: 120px;
+`
