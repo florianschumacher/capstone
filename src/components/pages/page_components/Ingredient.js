81aeaec65d10b2700467fd81d8cb58e7bcf0db4f
Florian Schumacher <fpsschumacher@gmail.com>
2020-12-10 22:02:26 +0100

    Styling Receipt Page
    Checkboxes, Tab Navigation, Font Stiling Information

--- src/components/pages/page_components/Ingredient.js
+++ src/components/pages/page_components/Ingredient.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Ingredient = ({ title, isDone }) => {
+const Ingredient = ({ title, isDone, theme }) => {
     return (
-        <ul>
-            <input type="checkbox" checked={isDone} />
-            {title}
-        </ul>
+        <CheckBoxWrapper >
+            <input type="checkbox" checked={isDone} id="ingredient" />
+            <label htmlFor="ingredient">
+                {title}
+            </label>
+        </CheckBoxWrapper>
     )
 }
 
@@ -14,4 +17,17 @@ Ingredient.propTypes = {
     isDone: PropTypes.bool
 }
 
+const CheckBoxWrapper = styled.li`
+align-items: flex-start;
+    display: flex;
+    margin-bottom: 0.75rem;
+input {
+    margin-right: 1rem;
+    transform: scale(2);
+}
+label {
+    display: inline-block;
+    width: 85%;
+}
+`
 export default Ingredient
